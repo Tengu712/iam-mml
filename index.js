a0979303d16a8f0b05277c270462ee6030305157
Tengu712 <genreihoutengu@gmail.com>
2024-02-28 11:58:07 +0900

    feat: create base UI

---
+++ index.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // get elements
+  const btnPlay = document.getElementById("play")
+  const taMML = document.getElementById("mml")
+
+  // click the play button
+  btnPlay.addEventListener("click", () => {
+    console.log("hey")
+  })
+})
