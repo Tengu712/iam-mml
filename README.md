0ea976948c1b81c6001de596829c711ed433b6da
Tengu712 <genreihoutengu@gmail.com>
2024-03-05 23:15:39 +0900

    feat: play an wave

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # IAM.mml
+
+## What is this?
+
+Online AM/FM Synthesizer for Generating and Playing WAV from MML.
--- index.js
+++ index.js
@@ -1,10 +1,45 @@
+const SAMPLE_RATE = 44100
+const PER_SAMPLE_RATE = 1 / SAMPLE_RATE
+const SIZE_OF_32BITS = 4
+
+// DEBUG
+function createWave() {
+  const length = SAMPLE_RATE * 2
+  const wave = new Float32Array(length)
+  for (let i = 0; i < length; ++i) {
+    wave[i] = Math.sin(2 * Math.PI * 440 * PER_SAMPLE_RATE * i)
+  }
+  return wave
+}
+
+function createAudioBuffer(audioContext, waveBuffers) {
+  const maxWaveBufferSize = waveBuffers.reduce((a, b) => Math.max(a.length, b.length)).length
+  const audioBuffer = audioContext.createBuffer(waveBuffers.length, maxWaveBufferSize, SAMPLE_RATE)
+  for (let i = 0; i < waveBuffers.length; ++i) {
+    audioBuffer.copyToChannel(waveBuffers[i], i)
+  }
+  return audioBuffer
+}
+
+function playAudioBuffer(audioContext, audioBuffer) {
+  const source = audioContext.createBufferSource()
+  source.buffer = audioBuffer
+  source.connect(audioContext.destination)
+  source.start()
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   // get elements
   const btnPlay = document.getElementById("play")
   const taMML = document.getElementById("mml")
 
-  // click the play button
+  // add the event listener when clicking the play button
   btnPlay.addEventListener("click", () => {
-    console.log("hey")
+    const wave = createWave()
+
+    const audioContext = new AudioContext()
+    const audioBuffer = createAudioBuffer(audioContext, [wave])
+
+    playAudioBuffer(audioContext, audioBuffer)
   })
 })
