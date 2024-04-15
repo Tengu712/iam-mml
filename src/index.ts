a67e25f8ff700ea524ea6bc976238c9cd5eb1a30
Tengu712 <genreihoutengu@gmail.com>
2024-04-15 19:30:11 +0900

    feat: show line numbers and change layout

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,20 @@ function getElementById<T>(id: string): T {
   return document.getElementById(id)! as T
 }
 
+function addLineNumbersEvent(ta: HTMLTextAreaElement, taNumbers: HTMLTextAreaElement) {
+  ta.addEventListener('input', () => {
+    const count = ta.value.split('\n').length
+    let text = ''
+    for (let i = 1; i < count + 1; ++i) {
+      text += i + '\n'
+    }
+    taNumbers.value = text
+    taNumbers.scrollTop = ta.scrollTop
+  })
+  ta.addEventListener('scroll', () => (taNumbers.scrollTop = ta.scrollTop))
+  taNumbers.value = '1\n'
+}
+
 function getMaxWaveSize(waves: Float32Array[]): number {
   let max = 0
   for (const wave of waves) {
@@ -33,6 +47,13 @@ document.addEventListener('DOMContentLoaded', () => {
   // get elements
   const btnPlay = getElementById<HTMLButtonElement>('play')
   const taMML = getElementById<HTMLTextAreaElement>('mml')
+  const taMMLNumbers = getElementById<HTMLTextAreaElement>('mml-numbers')
+  const taInst = getElementById<HTMLTextAreaElement>('inst')
+  const taInstNumbers = getElementById<HTMLTextAreaElement>('inst-numbers')
+
+  // add line numbers event
+  addLineNumbersEvent(taMML, taMMLNumbers)
+  addLineNumbersEvent(taInst, taInstNumbers)
 
   // add the event listener when clicking the play button
   btnPlay.addEventListener('click', () => {
