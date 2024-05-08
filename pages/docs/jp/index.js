02e018fe02132447b26a4f7daa1ec42b3aa0e9c6
Tengu712 <genreihoutengu@gmail.com>
2024-05-09 00:36:20 +0900

    feat: add instrument docs

--- pages/docs/jp/index.js
+++ pages/docs/jp/index.js
@@ -1,28 +1,35 @@
-document.addEventListener("DOMContentLoaded", () => {
-	const menu = document.getElementById("menu")
-	menu.innerHTML = `
-	  <div id="sidebar-wrapper">
-		  <div id="sidebar">
-			  <div>
-				  <b>IAM.mml Docs</b>
-				</div>
-				<br>
-			  <div>
-				  <b>一般</b><br>
-					　<a href="../about">IAM.mmlとは？</a><br>
-					　<a href="../ebnf">EBNF</a>
-				</div>
-				<br>
+function createMenu(path) {
+  const menu = document.getElementById("menu")
+  menu.innerHTML = `
+    <div id="sidebar-wrapper">
+      <div id="sidebar">
         <div>
-				  <b>コマンド</b><br>
-					　<a href="../note">音符</a><br>
-					　<a href="../length">標準音価</a><br>
-					　<a href="../key">調号</a><br>
-					　<a href="../octave">オクターブ</a><br>
-					　<a href="../tempo">テンポ</a><br>
-					　<a href="../volume">音量</a>
-				</div>
-			</div>
+          <b>IAM.mml Docs</b>
+        </div>
+        <br>
+        <div>
+          <b>一般</b><br>
+          　<a href="${path}../about">IAM.mmlとは？</a><br>
+          　<a href="${path}../ebnf">EBNF</a>
+        </div>
+        <br>
+        <div>
+          <b>コマンド</b><br>
+          　<a href="${path}../note">音符</a><br>
+          　<a href="${path}../length">標準音価</a><br>
+          　<a href="${path}../key">調号</a><br>
+          　<a href="${path}../octave">オクターブ</a><br>
+          　<a href="${path}../tempo">テンポ</a><br>
+          　<a href="${path}../volume">音量</a><br>
+          　<a href="${path}../instrument">音源</a>
+        </div>
+        <br>
+        <div>
+          <b>音源</b><br>
+          　<a href="${path}../instrument/theory">理論</a><br>
+          　<a href="${path}../instrument/grammer">文法</a>
+        </div>
+      </div>
     </div>
   `
-})
+}
