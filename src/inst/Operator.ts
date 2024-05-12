d63dca69bd1cbc7860a8e88e2f3d0d96aaacdd2b
Tengu712 <genreihoutengu@gmail.com>
2024-05-12 16:53:27 +0900

    feat: add a feedback

--- src/inst/Operator.ts
+++ src/inst/Operator.ts
@@ -9,6 +9,7 @@ export class Operator {
   private readonly d: number
   private readonly s: number
   private readonly r: number
+  private readonly fbl: number
   private readonly modulator: Operators | null
 
   public constructor(lines: Lines, indent: number) {
@@ -34,7 +35,7 @@ export class Operator {
           return Number(n)
         }
       })
-    if (params.length > 6) {
+    if (params.length > 7) {
       throw new Error(
         `[syntax error] The number of operator parameters is too many: ${line.ln} line.`
       )
@@ -50,10 +51,16 @@ export class Operator {
     this.d = params[3]
     this.s = params[4]
     this.r = params[5]
+    this.fbl = params.length === 7 ? Math.floor(params[6]) : 0
 
     this.modulator = null
     const nextLineIndent = lines.getIndent()
     if (nextLineIndent > indent) {
+      if (this.fbl > 0) {
+        throw new Error(
+          `[syntax error] The operator with feedback must be in the deepest indent: ${line.ln} line.`
+        )
+      }
       this.modulator = new Operators(lines, nextLineIndent)
     }
   }
@@ -66,19 +73,19 @@ export class Operator {
     const i = t * PER_SAMPLE_RATE
     const j = g * PER_SAMPLE_RATE
     const m = this.modulator?.run(f, g, t) ?? 0
-    const base = this.v * Math.sin(2 * Math.PI * f * this.f * i + m)
-    if (i < this.a) {
-      return (i / this.a) * base
+    let result = 0
+    for (let fblc = 0; fblc < this.fbl + 1; ++fblc) {
+      const base = this.v * Math.sin(2 * Math.PI * f * this.f * i + m + result)
+      if (i < this.a) {
+        result = (i / this.a) * base
+      } else if (i < j && i < this.a + this.d) {
+        result = (this.s + (1 - this.s) * (1 - (i - this.a) / this.d)) * base
+      } else if (i < j) {
+        result = this.s * base
+      } else if (i < j + this.r) {
+        result = this.s * (1 - (i - j) / this.r) * base
+      }
     }
-    if (i < j && i < this.a + this.d) {
-      return (this.s + (1 - this.s) * (1 - (i - this.a) / this.d)) * base
-    }
-    if (i < j) {
-      return this.s * base
-    }
-    if (i < j + this.r) {
-      return this.s * (1 - (i - j) / this.r) * base
-    }
-    return 0
+    return result
   }
 }
