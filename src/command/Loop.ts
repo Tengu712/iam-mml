f4b486f3ccdcdb49db97ffd5410eb6ace965ce3f
Tengu712 <genreihoutengu@gmail.com>
2024-05-25 21:10:43 +0900

    feat: add delimiter to loop command

--- src/command/Loop.ts
+++ src/command/Loop.ts
@@ -6,11 +6,20 @@ import type {Characters} from '@/parse/Characters'
 
 export class Loop implements ICommand {
   private readonly count: number
-  private readonly commands: Commands
+  private readonly former: Commands
+  private readonly latter: Commands | null
+  private readonly isDelimited: boolean
 
-  private constructor(count: number, commands: Commands) {
+  private constructor(
+    count: number,
+    former: Commands,
+    latter: Commands | null,
+    isDelimited: boolean
+  ) {
     this.count = count
-    this.commands = commands
+    this.former = former
+    this.latter = latter
+    this.isDelimited = isDelimited
   }
 
   public static from(chars: Characters): Loop | null {
@@ -23,16 +32,27 @@ export class Loop implements ICommand {
     const ln = first.ln
     const cn = first.cn
 
-    // '['
+    // "["
     const open = chars.eatChar(['['])
     if (open === null) {
       return null
     }
-    // (COMMANDS)
-    const commands = new Commands(chars, true)
-    if (commands.isEmpty()) {
+    // commands
+    const former = new Commands(chars, true)
+    if (former.isEmpty()) {
       throw new Error(
         `[syntax error] The loop command must contain at least one command: ${ln} line, ${cn} char.`
+      )
+    }
+    // [ ":"
+    const colonChar = chars.get()
+    const colon = chars.eatChar([':'])
+    const isDelimited = colon !== null
+    // commands ]
+    const latter = isDelimited ? new Commands(chars, true) : null
+    if (latter !== null && latter.isEmpty()) {
+      throw new Error(
+        `[syntax error] No command is found after ':': ${colonChar!.ln} line, ${colonChar!.cn} char.`
       )
     }
     // ']'
@@ -41,7 +61,7 @@ export class Loop implements ICommand {
     if (closeChar === null || close === null) {
       throw new Error(`[syntax error] The loop command is not closed: ${ln} line, ${cn} char.`)
     }
-    // (NATURAL)
+    // natural-number
     const count = chars.eatNatural(closeChar.ln)
     if (count === null) {
       throw new Error(
@@ -49,20 +69,32 @@ export class Loop implements ICommand {
       )
     }
 
-    return new Loop(count, commands)
+    return new Loop(count, former, latter, isDelimited)
   }
 
   public getCount(): number {
     return this.count
   }
 
-  public getCommands(): Commands {
-    return this.commands
+  public getFormer(): Commands {
+    return this.former
+  }
+
+  public getLatter(): Commands | null {
+    return this.latter
+  }
+
+  public getIsDelimited(): boolean {
+    return this.isDelimited
   }
 
   public eval(buffer: Buffer): void {
     for (let i = 0; i < this.count; ++i) {
-      this.commands.eval(buffer)
+      this.former.eval(buffer)
+      if (this.isDelimited && i === this.count - 1) {
+        break
+      }
+      this.latter?.eval(buffer)
     }
   }
 }
