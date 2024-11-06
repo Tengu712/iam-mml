a093304c32a2291e0f5f0268316ca30e6acf5bcb
Tengu712 <genreihoutengu@gmail.com>
2024-11-06 10:13:45 +0900

    feat: modified docs

--- pages/docs/jp/index.js
+++ pages/docs/jp/index.js
@@ -10,40 +10,14 @@ function createMenu(path) {
         <div>
           <b>一般</b><br>
           　<a href="${path}../about">IAM.mmlとは？</a><br>
-          　<a href="${path}../releasenote">リリースノート</a><br>
-          　<a href="${path}../ebnf">EBNF</a>
+          　<a href="${path}../releasenote">リリースノート</a>
         </div>
         <br>
         <div>
-          <b>学習</b><br>
-          　<a href="${path}../learning/screen/">画面の見方</a><br>
-          　<a href="${path}../learning/sample">サンプルコード</a><br>
-          　<a href="${path}../learning/tutorial-score">楽譜定義</a><br>
-          　<a href="${path}../learning/tutorial-inst">音源定義</a>
-        </div>
-        <br>
-        <div>
-          <b>楽譜</b><br>
-          　<a href="${path}../score/grammer">文法</a>
-        </div>
-        <br>
-        <div>
-          <b>コマンド</b><br>
-          　<a href="${path}../command/note">音符</a><br>
-          　<a href="${path}../command/length">標準音価</a><br>
-          　<a href="${path}../command/key">調号</a><br>
-          　<a href="${path}../command/octave">オクターブ</a><br>
-          　<a href="${path}../command/tempo">テンポ</a><br>
-          　<a href="${path}../command/volume">音量</a><br>
-          　<a href="${path}../command/instrument">音源</a><br>
-          　<a href="${path}../command/macro">マクロ</a><br>
-          　<a href="${path}../command/loop">ループ</a>
-        </div>
-        <br>
-        <div>
-          <b>音源</b><br>
-          　<a href="${path}../instrument/theory">理論</a><br>
-          　<a href="${path}../instrument/grammer">文法</a>
+          <b>一覧</b><br>
+          　<a href="${path}../reference/macro-inst">マクロと楽器</a><br>
+          　<a href="${path}../reference/directive">ディレクティブ</a><br>
+          　<a href="${path}../reference/command">コマンド</a>
         </div>
         <br>
         <a href="https://github.com/Tengu712/iam-mml">GitHub</a>
