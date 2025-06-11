9efa5b214267f9e7ebd65cbce2928f2bca6f21c4
Tengu712 <genreihoutengu@gmail.com>
2025-06-12 01:00:23 +0900

    css

--- pages/src/components/DocumentPage.jsx
+++ pages/src/components/DocumentPage.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
 import { Link } from 'react-router-dom'
+import style from './DocumentPage.module.css'
 
 const mdxModules = Object.entries(import.meta.glob('../pages/**/*.mdx')).map(([path, module]) => ({ module: module, path: path.slice(9, -4) }))
 
@@ -14,6 +15,7 @@ function DocumentPage({ children }) {
   useEffect(() => {
     setSidebarOpen(false)
     setHasError(false)
+    window.scrollTo(0, 0);
     const path = location.pathname.replace(/^\/|\/$/g, '') || 'index'
     mdxModules.find((n) => n.path === path)?.module()
         .then((n) => setMdxComponent(() => n.default))
@@ -24,32 +26,32 @@ function DocumentPage({ children }) {
   return hasError
     ? (<div>Page not found.</div>)
     : (
-      <div className='app'>
-        <div className='layout'>
-          <aside className={`sidebar ${sidebarOpen ? 'open' : ''}`}>
+      <div className={style.app}>
+        <div className={style.layout}>
+          <aside className={`${style.sidebar} ${sidebarOpen ? style.open : ''}`}>
             <nav>
               <div>
                 <label>General</label>
               </div>
-              <Link to='/docs/jp/about/'        className='sidebar-link nested'>Introduction</Link>
-              <Link to='/docs/jp/release-note/' className='sidebar-link nested'>Release Note</Link>
+              <Link to='/docs/jp/about/'        className={style.sidebarLink}>Introduction</Link>
+              <Link to='/docs/jp/release-note/' className={style.sidebarLink}>Release Note</Link>
               <div>
                 <label>Tutorial</label>
               </div>
-              <Link to='/docs/jp/tutorial/cdefgab/'    className='sidebar-link nested'>1. CDEFGAB</Link>
-              <Link to='/docs/jp/tutorial/commands/'   className='sidebar-link nested'>2. Commands</Link>
-              <Link to='/docs/jp/tutorial/directives/' className='sidebar-link nested'>3. Directives</Link>
-              <Link to='/docs/jp/tutorial/instrument/' className='sidebar-link nested'>4. Instrument</Link>
+              <Link to='/docs/jp/tutorial/cdefgab/'    className={style.sidebarLink}>1. CDEFGAB</Link>
+              <Link to='/docs/jp/tutorial/commands/'   className={style.sidebarLink}>2. Commands</Link>
+              <Link to='/docs/jp/tutorial/directives/' className={style.sidebarLink}>3. Directives</Link>
+              <Link to='/docs/jp/tutorial/instrument/' className={style.sidebarLink}>4. Instrument</Link>
               <div>
                 <label>Reference</label>
               </div>
-              <Link to='/docs/jp/reference/macro-inst/' className='sidebar-link nested'>Macro & Instrument</Link>
-              <Link to='/docs/jp/reference/directives/' className='sidebar-link nested'>Directives</Link>
-              <Link to='/docs/jp/reference/commands/' className='sidebar-link nested'>Commands</Link>
+              <Link to='/docs/jp/reference/macro-inst/' className={style.sidebarLink}>Macro & Instrument</Link>
+              <Link to='/docs/jp/reference/directives/' className={style.sidebarLink}>Directives</Link>
+              <Link to='/docs/jp/reference/commands/' className={style.sidebarLink}>Commands</Link>
               <div>
                 <label>Theory</label>
               </div>
-              <Link to='/docs/jp/theory/synthesize/' className='sidebar-link nested'>Synthesize</Link>
+              <Link to='/docs/jp/theory/synthesize/' className={style.sidebarLink}>Synthesize</Link>
               <div>
                 <a href='https://github.com/Tengu712/iam-mml'>
                   <img src='/iam-mml/github-mark-white.svg' alt='GitHub' />
@@ -57,17 +59,17 @@ function DocumentPage({ children }) {
               </div>
             </nav>
           </aside>
-          <div className={`main-container ${sidebarOpen ? 'sidebar-open' : ''}`}>
-            <header className='header'>
-              <div className='header-content'>
-                <button className='menu-toggle' onClick={() => setSidebarOpen((prev) => !prev)}>
+          <div className={`${style.mainContainer} ${sidebarOpen ? style.sidebarOpen : ''}`}>
+            <header className={style.header}>
+              <div className={style.headerContent}>
+                <button className={style.menuToggle} onClick={() => setSidebarOpen((prev) => !prev)}>
                   ☰
                 </button>
-                <div className='logo'>IAM.mml Docs</div>
+                <div className={style.logo}>IAM.mml Docs</div>
               </div>
             </header>
-            <main className='main-content'>
-              <div className='mdx-content'>
+            <main className={style.mainContent}>
+              <div className={style.mdxContent}>
                 {MdxComponent && <MdxComponent />}
               </div>
             </main>
--- pages/src/pages/index.jsx
+++ pages/src/pages/index.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState, useRef } from 'react'
+import { Link } from 'react-router-dom'
 import playIcon from '../assets/play.svg'
 import pauseIcon from '../assets/pause.svg'
 import stopIcon from '../assets/stop.svg'
 import buildIcon from '../assets/build.svg'
 import infoIcon from '../assets/info.svg'
-import './index.css'
+import styles from './index.module.css'
 
 function PlaygroundPage() {
   const wasmLoading = useRef(true)
@@ -23,34 +24,36 @@ function PlaygroundPage() {
   }, [])
 
   return (
-    <div id="main" className="playground-page">
-      <div id="button-wrapper">
-        <button id="play" className="red-button" title="Play the music">
+    <div className={styles.main}>
+      <div className={styles.buttonWrapper}>
+        <button id="play" className={styles.redButton} title="Play the music">
           <img src={playIcon} />
         </button>
-        <button id="pause" className="red-button" title="Pause the music">
+        <button id="pause" className={styles.redButton} title="Pause the music">
           <img src={pauseIcon} />
         </button>
-        <button id="stop" className="red-button" title="Stop the music">
+        <button id="stop" className={`${styles.redButton} ${styles.stop}`} title="Stop the music">
           <img src={stopIcon} />
         </button>
-        <button id="build" className="white-button" title="Build and download the music">
+        <button id="build" className={styles.whiteButton} title="Build and download the music">
           <img src={buildIcon} />
         </button>
-        <button id="info" className="white-button" title="Go to IAM.mml Docs">
-          <img src={infoIcon} />
+        <button className={`${styles.whiteButton} ${styles.info}`} title="Go to IAM.mml Docs">
+          <Link to="/docs/jp/about/">
+            <img src={infoIcon} />
+          </Link>
         </button>
       </div>
 
-      <div id="textarea-wrapper">
-        <div id="textarea-upper">
-          <div id="mml-wrapper">
-            <textarea id="mml-numbers" readOnly></textarea>
-            <textarea id="mml" spellCheck="false"></textarea>
+      <div className={styles.textareaWrapper}>
+        <div className={styles.textareaUpper}>
+          <div id="mml-wrapper" className={styles.mmlWrapper}>
+            <textarea id="mml-numbers" className={styles.mmlNumbers} readOnly></textarea>
+            <textarea id="mml" className={styles.mml} spellCheck="false"></textarea>
           </div>
-          <div id="textarea-resizer"></div>
-          <div id="log-wrapper">
-            <textarea id="log" spellCheck="false"></textarea>
+          <div id="textarea-resizer" className={styles.textareaResizer}></div>
+          <div className={styles.logWrapper}>
+            <textarea id="log" className={styles.log} spellCheck="false"></textarea>
           </div>
         </div>
       </div>
